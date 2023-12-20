0e385b2e8e5f1392d9fae3bd729ed3239ddbce1a
Nikhil Gupta <85886260+jcodeNikhil@users.noreply.github.com>
2023-12-20 17:35:12 +0530

    connect and sign a message

--- app/src/main/java/com/dev/frequenc/ui_codes/screens/Dashboard/wallet/WalletFragment.kt
+++ app/src/main/java/com/dev/frequenc/ui_codes/screens/Dashboard/wallet/WalletFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.dev.frequenc.databinding.FragmentWalletBinding
 import com.dev.frequenc.ui_codes.MainActivity
@@ -18,10 +19,13 @@ import io.metamask.androidsdk.RequestError
 import io.metamask.androidsdk.TAG
 
 
+@AndroidEntryPoint
 class WalletFragment : Fragment() {
 
 
     private lateinit var walletBinding: FragmentWalletBinding
+    private val viewModel by viewModels<WalletViewModel>()
+    val dapp = Dapp("Droid Dapp", "https://droiddapp.com")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,8 +41,47 @@ class WalletFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        bindHandlers()
+        bindObservers()
 
+    }
 
+    private fun bindHandlers() {
+        walletBinding.apply {
+            btnConnect.setOnClickListener {
+                viewModel.connect(dapp) { result ->
+                    if (result is RequestError) {
+                        Log.e(TAG, "Ethereum connection error: ${result.message}")
+                    } else {
+                        Log.d(TAG, "Ethereum connection result: $result")
+                        viewModel.sendRequest { result2 ->
+                            if (result2 is RequestError) {
+                                Log.e(TAG, "Ethereum sign error: ${result2.message}")
+                            } else {
+                                Log.d(TAG, "Ethereum sign result: $result2")
+                            }
+                        }
+                    }
+                }
+            }
+            btnBuy.setOnClickListener {
+                viewModel.sendTransaction { result ->
+                    if (result is RequestError) {
+                        // handle error
+                    } else {
+                        Log.d(TAG, "Ethereum transaction result: $result")
+                    }
+                }
+            }
+        }
+    }
+
+    private fun bindObservers() {
+        viewModel.ethereumState.observe(viewLifecycleOwner) {
+            if (it.selectedAddress.isNotEmpty()) {
+                walletBinding.tvAddress.text = it.toString()
+            }
+        }
     }
 
 
--- app/src/main/java/com/dev/frequenc/ui_codes/screens/Dashboard/wallet/WalletViewModel.kt
+++ app/src/main/java/com/dev/frequenc/ui_codes/screens/Dashboard/wallet/WalletViewModel.kt
@@ -56,8 +56,12 @@ class WalletViewModel @Inject constructor(
     // Wrapper function call all RPC methods
     fun sendRequest(callback: ((Any?) -> Unit)?) {
         viewModelScope.launch {
-            val message =
-                "{\"domain\":{\"chainId\":\"${ethereum.chainId}\",\"name\":\"Ether Mail\",\"verifyingContract\":\"0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC\",\"version\":\"1\"},\"message\":{\"contents\":\"Hello, Mahesh Babu !\",\"from\":{\"name\":\"Kinno\",\"wallets\":[\"0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826\",\"0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF\"]},\"to\":[{\"name\":\"Busa\",\"wallets\":[\"0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB\",\"0xB0BdaBea57B0BDABeA57b0bdABEA57b0BDabEa57\",\"0xB0B0b0b0b0b0B000000000000000000000000000\"]}]},\"primaryType\":\"Mail\",\"types\":{\"EIP712Domain\":[{\"name\":\"name\",\"type\":\"string\"},{\"name\":\"version\",\"type\":\"string\"},{\"name\":\"chainId\",\"type\":\"uint256\"},{\"name\":\"verifyingContract\",\"type\":\"address\"}],\"Group\":[{\"name\":\"name\",\"type\":\"string\"},{\"name\":\"members\",\"type\":\"Person[]\"}],\"Mail\":[{\"name\":\"from\",\"type\":\"Person\"},{\"name\":\"to\",\"type\":\"Person[]\"},{\"name\":\"contents\",\"type\":\"string\"}],\"Person\":[{\"name\":\"name\",\"type\":\"string\"},{\"name\":\"wallets\",\"type\":\"address[]\"}]}}"
+//            val message =
+//                "{\"domain\":{\"chainId\":\"${ethereum.chainId}\",\"name\":\"Ether Mail\",\"verifyingContract\":\"0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC\",\"version\":\"1\"},\"message\":{\"contents\":\"Hello, Mahesh Babu !\",\"from\":{\"name\":\"Kinno\",\"wallets\":[\"0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826\",\"0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF\"]},\"to\":[{\"name\":\"Busa\",\"wallets\":[\"0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB\",\"0xB0BdaBea57B0BDABeA57b0bdABEA57b0BDabEa57\",\"0xB0B0b0b0b0b0B000000000000000000000000000\"]}]},\"primaryType\":\"Mail\",\"types\":{\"EIP712Domain\":[{\"name\":\"name\",\"type\":\"string\"},{\"name\":\"version\",\"type\":\"string\"},{\"name\":\"chainId\",\"type\":\"uint256\"},{\"name\":\"verifyingContract\",\"type\":\"address\"}],\"Group\":[{\"name\":\"name\",\"type\":\"string\"},{\"name\":\"members\",\"type\":\"Person[]\"}],\"Mail\":[{\"name\":\"from\",\"type\":\"Person\"},{\"name\":\"to\",\"type\":\"Person[]\"},{\"name\":\"contents\",\"type\":\"string\"}],\"Person\":[{\"name\":\"name\",\"type\":\"string\"},{\"name\":\"wallets\",\"type\":\"address[]\"}]}}"
+          val message =
+                "{\"domain\":{\"chainId\":\"${ethereum.chainId}\",\"name\":\"Ether Mail\",\"verifyingContract\":\"0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC\",\"version\":\"1\"},\"message\":{\"contents\":\"Login signature\",\"from\":{\"name\":\"Kinno\",\"wallets\":[\"0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826\"]},\"to\":[{\"name\":\"Busa\",\"wallets\":[\"0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB\",\"0xB0BdaBea57B0BDABeA57b0bdABEA57b0BDabEa57\",\"0xB0B0b0b0b0b0B000000000000000000000000000\"]}]},\"primaryType\":\"Mail\",\"types\":{\"EIP712Domain\":[{\"name\":\"name\",\"type\":\"string\"},{\"name\":\"version\",\"type\":\"string\"},{\"name\":\"chainId\",\"type\":\"uint256\"},{\"name\":\"verifyingContract\",\"type\":\"address\"}],\"Group\":[{\"name\":\"name\",\"type\":\"string\"},{\"name\":\"members\",\"type\":\"Person[]\"}],\"Mail\":[{\"name\":\"from\",\"type\":\"Person\"},{\"name\":\"to\",\"type\":\"Person[]\"},{\"name\":\"contents\",\"type\":\"string\"}],\"Person\":[{\"name\":\"name\",\"type\":\"string\"},{\"name\":\"wallets\",\"type\":\"address[]\"}]}}"
+//            val message =
+//                "{\"domain\":{\"chainId\":\"${ethereum.chainId}\",\"name\":\"Ether Mail\",\"verifyingContract\":\"0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC\",\"version\":\"1\"},\"message\":{\"contents\":\"Login signature\"},\"primaryType\":\"Mail\",\"types\":{\"EIP712Domain\":[{\"name\":\"name\",\"type\":\"string\"},{\"name\":\"version\",\"type\":\"string\"},{\"name\":\"chainId\",\"type\":\"uint256\"},{\"name\":\"verifyingContract\",\"type\":\"address\"}],\"Group\":[{\"name\":\"name\",\"type\":\"string\"},{\"name\":\"members\",\"type\":\"Person[]\"}],\"Mail\":[{\"name\":\"from\",\"type\":\"Person\"},{\"name\":\"to\",\"type\":\"Person[]\"},{\"name\":\"contents\",\"type\":\"string\"}],\"Person\":[{\"name\":\"name\",\"type\":\"string\"},{\"name\":\"wallets\",\"type\":\"address[]\"}]}}"
             val from = ethereum.selectedAddress
             val params: List<String> = listOf(from, message)
 
@@ -103,8 +107,9 @@ class WalletViewModel @Inject constructor(
     fun sendTransaction(callback: ((Any?) -> Unit)?) {
         viewModelScope.launch {
             val from = ethereum.selectedAddress
-            val to = "0x0000000000000000000000000000000000000000"
-            val amount = "0x01"
+//            val to = "0x0000000000000000000000000000000000000000"
+            val to = "0x2f930D27e0502Ef690A418D03CF037d0509000c7"
+            val amount = "0x5"
             val params: Map<String, Any> = mapOf(
                 "from" to from,
                 "to" to to,
